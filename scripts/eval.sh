cee6739595fbb73ba82c71330a0f3f769471d5ee
Mehdi Benchikh <mb10856@cn120.fast>
2025-11-10 00:02:54 +0400

    add scripts

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -2,12 +2,16 @@
 
 # Define the resource requirements here using #SBATCH
 
+#SBATCH -J eval-p-original
 #SBATCH -p compute
+#SBATCH --reservation=scomputer-dalma
+#SBATCH --exclusive
 #SBATCH --nodes=1
-#SBATCH -c 4
-#SBATCH --mem=16G
-#SBATCH -t 07-00
-#SBATCH -o logs/neptune/%j.out
+#SBATCH -c 28
+#SBATCH --mem=100G
+#SBATCH -t 7-00
+#SBATCH -o logs/%x_%j.out
+#SBATCH -e logs/%x_%j.err
 #SBATCH --mail-type=FAIL,TIME_LIMIT
 #SBATCH --mail-user=mb10856@nyu.edu
 
@@ -24,5 +28,9 @@ conda activate mlir
 cd "$(dirname "$0")/.." && PROJECT_ROOT="$(pwd)"
 
 # Execute the code
-python experiments/neptune_sync.py
+export EVAL_DIR=results/run
+export DASK_NODES=0
+export OMP_NUM_THREADS=12
+export CONFIG_FILE_PATH="${PROJECT_ROOT}/config/config.json"
+python bin/evaluate.py
 
